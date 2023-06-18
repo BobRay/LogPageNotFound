62517fe3ada36ee61444ab09fda28f528026875c
bobray <bobray@softville.com>
2023-06-18 17:25:41 -0500

    Clarify code; Add get_browser_name(); Truncate cell values; Move log to cache

--- core/components/logpagenotfound/elements/plugins/logpagenotfound.plugin.php
+++ core/components/logpagenotfound/elements/plugins/logpagenotfound.plugin.php
@@ -36,12 +36,12 @@ if (!function_exists("get_host")) {
     }
 }
 if (!function_exists("logLine")) {
-    function logLine($bLogLine, $maxLines, $file)
+    function logLine($line, $maxLines, $file)
     {
 
-        if ($bLogLine) {
-            $bLogLine = strip_tags($bLogLine);
-            $bLogLine = htmlspecialchars($bLogLine, ENT_QUOTES, 'UTF-8');
+        if ($line) {
+            $line = strip_tags($line);
+            $line = htmlspecialchars($line, ENT_QUOTES, 'UTF-8');
 
             $log = file($file);
             if ($fp = fopen($file, 'a')) { // tiny danger of 2 threads interfering; live with it
@@ -51,38 +51,140 @@ if (!function_exists("logLine")) {
                     {
                         array_shift($log);
                     }
-                    array_push($log, $bLogLine);
-                    $bLogLine = implode('', $log);
+                    array_push($log, $line);
+                    $line = implode('', $log);
                     $fp = fopen($file, 'w');
                 }
-                fputs($fp, $bLogLine);
+                fwrite($fp, $line);
                 fclose($fp);
             }
             return;
         }
     }
 }
+/* Function courtesy of Anonymous at php.net */
+if (! function_exists('get_browser_name')) {
+    function get_browser_name($user_agent) {
+        // Make case-insensitive.
+        $t = strtolower($user_agent);
+
+        // If the string *starts* with the string, strpos returns 0 (i.e., FALSE). Do a ghetto hack and start with a space.
+        // "[strpos()] may return Boolean FALSE, but may also return a non-Boolean value which evaluates to FALSE."
+        //     http://php.net/manual/en/function.strpos.php
+        $t = " " . $t;
+
+        // Humans / Regular Users
+        if (strpos($t, 'opera') || strpos($t, 'opr/')) {
+            return 'Opera';
+        } elseif (strpos($t, 'edge')) {
+            return 'Edge';
+        } elseif (strpos($t, 'chrome')) {
+            return 'Chrome';
+        } elseif (strpos($t, 'safari')) {
+            return 'Safari';
+        } elseif (strpos($t, 'firefox')) {
+            return 'Firefox';
+        } elseif (strpos($t, 'msie') || strpos($t, 'trident/7')) {
+            return 'Internet Explorer';
+        } // Search Engines
+        elseif (strpos($t, 'google')) {
+            return '[Bot] Googlebot';
+        } elseif (strpos($t, 'bing')) {
+            return '[Bot] Bingbot';
+        } elseif (strpos($t, 'slurp')) {
+            return '[Bot] Yahoo! Slurp';
+        } elseif (strpos($t, 'duckduckgo')) {
+            return '[Bot] DuckDuckBot';
+        } elseif (strpos($t, 'baidu')) {
+            return '[Bot] Baidu';
+        } elseif (strpos($t, 'yandex')) {
+            return '[Bot] Yandex';
+        } elseif (strpos($t, 'sogou')) {
+            return '[Bot] Sogou';
+        } elseif (strpos($t, 'exabot')) {
+            return '[Bot] Exabot';
+        } elseif (strpos($t, 'msn')) {
+            return '[Bot] MSN';
+        } // Common Tools and Bots
+        elseif (strpos($t, 'mj12bot')) {
+            return '[Bot] Majestic';
+        } elseif (strpos($t, 'ahrefs')) {
+            return '[Bot] Ahrefs';
+        } elseif (strpos($t, 'semrush')) {
+            return '[Bot] SEMRush';
+        } elseif (strpos($t, 'rogerbot') || strpos($t, 'dotbot')) {
+            return '[Bot] Moz or OpenSiteExplorer';
+        } elseif (strpos($t, 'frog') || strpos($t, 'screaming')) {
+            return '[Bot] Screaming Frog';
+        } // Miscellaneous
+        elseif (strpos($t, 'facebook')) {
+            return '[Bot] Facebook';
+        } elseif (strpos($t, 'pinterest')) {
+            return '[Bot] Pinterest';
+        } // Check for strings commonly used in bot user agents
+        elseif (strpos($t, 'crawler') || strpos($t, 'api') ||
+            strpos($t, 'spider') || strpos($t, 'http') ||
+            strpos($t, 'bot') || strpos($t, 'archive') ||
+            strpos($t, 'info') || strpos($t, 'data')) {
+            return '[Bot] Other';
+        }
+        return 'Other (Unknown)';
+    }
+}
+
+
 /* Don't execute in Manager */
 /** @var $modx modX */
 /** @var $scriptProperties array */
 if ($modx->context->get('key') == 'mgr') {
     return '';
 }
-$oldSetting = ignore_user_abort(TRUE); // otherwise can screw-up logfile
-$data['page'] = $_SERVER['REQUEST_URI'];
-$t = gettimeofday();
-$data['time'] = date('d/m/y H:i:s:') . substr($t['usec'], 0, 3); // H:i:s:u
-$data['ip'] = $_SERVER['REMOTE_ADDR'];
-$data['userAgent'] = isset($_SERVER['HTTP_USER_AGENT'])
-        ? $_SERVER['HTTP_USER_AGENT']
-        : '<unknown user agent>';
+$oldSetting = ignore_user_abort(TRUE); // otherwise can screw up logfile
 
-$data['host'] = get_host($data['ip']);
+$modx->log(modX::LOG_LEVEL_ERROR, 'REQUEST_URI: ' . $_SERVER['REQUEST_URI']);
+$modx->log(modX::LOG_LEVEL_ERROR, 'base_url: : ' . $modx->getOption('base_url'));
+$page = $_SERVER['REQUEST_URI'];
+
+/* Remove site url prefix */
+$siteUrl = $modx->getOption('base_url');
+$page = (strpos($page, $siteUrl) === 0)
+    ? substr($page, strlen($siteUrl))
+    : $page;
+
+$data['page'] = $page;
+$t = gettimeofday();
+$data['time'] = date('m/d/y H:i:s:');
+
+$ip = $_SERVER['REMOTE_ADDR'];
+if (strpos($ip, ',') !== false) {
+    $ips = explode(', ', $ip);
+    $ip = $ips[0];
+}
+if ($ip == '::1') {
+    $ip = 'localhost';
+}
+$data['ip'] = $ip;
+/*$data['userAgent'] = isset($_SERVER['HTTP_USER_AGENT'])
+        ? $_SERVER['HTTP_USER_AGENT']
+        : '<unknown user agent>';*/
+$data['userAgent'] = isset($_SERVER['HTTP_USER_AGENT'])
+   ? get_browser_name($_SERVER['HTTP_USER_AGENT'])
+   : 'unknown';
+
+// $data['host'] = get_host($data['ip']);
+
+$data['host'] = gethostbyaddr($data['ip']);
+
+/* Set to unknown if gethostbyaddr() returns IP, or false */
+if (($data['host'] == $data['ip']) || (!$data['host'])) {
+    $data['host'] = 'Unknown';
+}
+
 $data['referer'] = empty($_SERVER['HTTP_REFERER']) ? '(empty)' : $_SERVER['HTTP_REFERER'];
 $msg = implode('`', $data);
 
 $maxLines  = $modx->getOption('log_max_lines',$scriptProperties, 300);
-$file = $modx->getOption('log_path', $scriptProperties, MODX_CORE_PATH . 'logs/pagenotfound.log', true );
+$file = $modx->getOption('log_path', $scriptProperties, MODX_CORE_PATH . 'cache/logs/pagenotfound.log', true );
 
 logLine($msg . "\n", $maxLines, $file);
 
